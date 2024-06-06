ad183d7d341c19b32224cd9c74f3a8f1d4a4d23c
Chea Chanbo <cbo75920@gmail.com>
2024-06-06 22:41:42 +0700

    fist commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector("label").innerHTML = randomNumber1;
+// document.getElementsById("number1").innerHTML = randomNumber1;
+// document.getElementsById("number2").innerHTML = randomNumber2;
+
+document.getElementById("one").innerHTML = randomNumber1;
+document.getElementById("two").innerHTML = randomNumber2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
